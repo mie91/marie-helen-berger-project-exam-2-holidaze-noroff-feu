2aba2de6317b9c023cbd482d4f7ec7cd3a3741d7
Marie Helen Berger <44783768+mie91@users.noreply.github.com>
2020-05-20 16:43:53 +0200

    Finished main content for "Home" page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,34 @@
-import React from "react";
-import {Container, Col, Row} from "react-bootstrap";
+import React, { useState } from "react";
+import {Container, Col, Row, Form, Button} from "react-bootstrap";
 import BackToTop from "../layout/other/BackToTop";
 import MainHeader from "../layout/headers/MainHeader";
 import SubHeader from "../layout/headers/SubHeader";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
 
+
+const schema = yup.object().shape({
+
+    email: yup
+        .string()
+        .email("Please enter a valid email")
+        .required("Email address is required")
+
+});
 
 function Home() {
+
+    const [validated, setValidated] = useState(false);
+    const { register, handleSubmit, errors } = useForm({
+        validationSchema: schema
+    });
+
+    function onSubmit(data) {
+        console.log("data", data);
+        setValidated(true);
+    }
+
     return (
         <>
             <div className="header-image header-image--home">
@@ -33,6 +55,26 @@ function Home() {
                             <SubHeader title="Finding the perfect place"/>
                             <p>Do aliqua sit ex consequat culpa deserunt id. In excepteur aute nisi cupidatat esse aliquip tempor ad cillum consectetur velit do. Occaecat nulla quis elit enim pariatur amet esse ad consectetur. Laborum consequat laborum tempor qui et est proident ex. Amet cillum voluptate est pariatur et excepteur enim labore irure. Sit amet consectetur proident reprehenderit adipisicing. Commodo ut fugiat nisi veniam incididunt voluptate deserunt ea incididunt qui excepteur voluptate quis.
                         </p>
+                        </div>
+                    </Col>
+                </Row>
+                    <Row className="justify-content-md-center">
+                    <Col className="col-sm-12">
+                        <div className="newsletter-home">
+                            <MainHeader title="Sign up for our newsletter"/>
+                            <SubHeader title="Get the latest news, and unique deals!"/>
+                            
+                                <Form className="newsletter-home__form" inline onSubmit={handleSubmit(onSubmit)}>
+                                        
+                                    <Form.Control className="newsletter-home__input" name="email" placeholder="Enter your E-mail" ref={register} />
+
+                                    <Button className="newsletter-home__btn" type="submit">Sign up!</Button>
+   
+                                    </Form>
+                                    {errors.email && <div className="newsletter-home__error">{errors.email.message}</div>}
+
+                                    {validated && <div className="newsletter-home__validated">You are now signed up successfully!</div>}
+                
                         </div>
                     </Col>
                 </Row>
