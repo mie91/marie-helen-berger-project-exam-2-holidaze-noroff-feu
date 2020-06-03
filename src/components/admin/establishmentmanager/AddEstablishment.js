18a5172222929348a749c7df2ba32e40209fe4c3
Marie Helen Berger <44783768+mie91@users.noreply.github.com>
2020-06-03 16:40:55 +0200

    Added base for add establishment
    Needs more work and tuning

--- src/components/admin/establishmentmanager/AddEstablishment.js
+++ src/components/admin/establishmentmanager/AddEstablishment.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { useHistory } from "react-router-dom";
-import { Button, Form } from "react-bootstrap";
-import { BASE_URL, headers } from "../../../constants/api";
+import * as yup from "yup";
+import { useParams, useHistory } from "react-router-dom";
+import { Container, Row, Col, Image, Form } from "react-bootstrap";
+import { BASE_URL, headers, PATCH } from "../../../constants/api";
+import DeleteEstablishment from "./DeleteEstablishment";
+import ErrorMessage from "../../layout/other/ErrorMessage";
+import Logo from "../../../assets/logo/logoLight_m.png";
+import { Link } from "react-router-dom";
+
+const schema = yup.object().shape({
+  name: yup
+    .string()
+    .required("A name is required")
+    .min(2, "The name is to short"),
+
+});
+
 
 function AddEstablishment() {
-    const { register, handleSubmit } = useForm();
-
+    const { register, handleSubmit, errors } = useForm({
+      validationSchema: schema,
+    });
+    const [validated, setValidated] = useState(false);
     const history = useHistory();
 
     async function onSubmit(data) {
@@ -17,12 +33,238 @@ function AddEstablishment() {
         const options = { headers, method: "POST", body: JSON.stringify(data) };
 
         await fetch(url, options);
-
+        setValidated(true);
+        setTimeout(() => {
         history.push("/admin/establishments");
+        }, 2000);
+        
     }
 
     return (
-        <Form onSubmit={handleSubmit(onSubmit)}>
+        <>
+      <div className="main-container main-container--white-purple main-container main-container--admin">
+        <Container>
+          <div className="basic-btn basic-btn--dark">
+            <Link to={"/admin/establishments"}>back </Link>{" "}
+          </div>{" "}
+          <div className="admin-box admin-box--establishment-edit">
+            <Row className="justify-content-md-center">
+              <Col lg={3} md={10} sm={12}>
+                <div className="admin-box__image">
+                  <Image src={Logo} fluid></Image>
+                </div>
+              </Col>
+            </Row>
+            <Row className="justify-content-md-center">
+              <Col lg={8} md={10} sm={12}>
+                <h1 className="admin-box__header">Add Establishment</h1>
+                
+              </Col>
+            </Row>
+            <Row className="justify-content-md-center">
+              <Col lg={10} md={10} sm={12}>
+                <div className="admin-box__form">
+                  <Form onSubmit={handleSubmit(onSubmit)}>
+                    <Row className="justify-content-md-center">
+                      <Col lg={10} md={12} sm={12}>
+                        <Form.Group className="admin-box__form-group">
+                          <Form.Label className="admin-box__form-label">
+                            Name:
+                          </Form.Label>
+                          <Form.Control
+                            className="admin-box__form-input"
+                            name="name"
+                            
+                            placeholder="Enter name of establishment"
+                            ref={register}
+                          />
+                          {errors.name && (
+                            <ErrorMessage>{errors.name.message}</ErrorMessage>
+                          )}
+                        </Form.Group>
+                      </Col>
+                      
+                    </Row>
+                    <Row className="justify-content-md-center">
+                      <Col lg={6} md={12} sm={12}>
+                        <Form.Group className="admin-box__form-group">
+                          <Form.Label className="admin-box__form-label">
+                            Email:
+                          </Form.Label>
+                          <Form.Control
+                            className="admin-box__form-input"
+                            name="email"
+                            
+                            placeholder="Enter establishment email"
+                            ref={register}
+                          />
+                          {errors.email && (
+                            <ErrorMessage>{errors.email.message}</ErrorMessage>
+                          )}
+                        </Form.Group>
+                      </Col>
+                      <Col lg={4} md={12} sm={12}>
+                        <Form.Group className="admin-box__form-group">
+                          <Form.Label className="admin-box__form-label">
+                            Price:
+                          </Form.Label>
+                          <Form.Control
+                            className="admin-box__form-input"
+                            name="price"
+                            
+                            placeholder="Enter price per night"
+                            ref={register}
+                          />
+                          {errors.price && (
+                            <ErrorMessage>{errors.price.message}</ErrorMessage>
+                          )}
+                        </Form.Group>
+                      </Col>
+                    </Row>
+                    <Row className="justify-content-md-center">
+                      <Col lg={6} md={12} sm={12}>
+                        <Form.Group className="admin-box__form-group">
+                          <Form.Label className="admin-box__form-label">
+                            Image:
+                          </Form.Label>
+                          <Form.Control
+                            className="admin-box__form-input"
+                            name="image"
+                            
+                            placeholder="Enter image URL"
+                            ref={register}
+                          />
+                        </Form.Group>
+                      </Col>
+                      <Col lg={4} md={12} sm={12}>
+                        <Form.Group className="admin-box__form-group">
+                          <Form.Label className="admin-box__form-label">
+                            Max guests:
+                          </Form.Label>
+                          <Form.Control
+                            className="admin-box__form-input"
+                            name="maxGuests"
+                            
+                            placeholder="Max guests"
+                            ref={register}
+                          />
+                          {errors.maxGuests && (
+                            <ErrorMessage>
+                              {errors.maxGuests.message}
+                            </ErrorMessage>
+                          )}
+                        </Form.Group>
+                      </Col>
+                    </Row>
+                    <Row className="justify-content-md-center">
+                      <Col lg={3} md={12} sm={12}>
+                        <Form.Group className="admin-box__form-group">
+                          <Form.Label className="admin-box__form-label">
+                            Latitude:
+                          </Form.Label>
+                          <Form.Control
+                            className="admin-box__form-input"
+                            name="lat"
+                            d
+                            placeholder="Enter coordinate (latitude)"
+                            ref={register}
+                          />
+                          {errors.lat && (
+                            <ErrorMessage>{errors.lat.message}</ErrorMessage>
+                          )}
+                        </Form.Group>
+                      </Col>
+                      <Col lg={3} md={12} sm={12}>
+                        <Form.Group className="admin-box__form-group">
+                          <Form.Label className="admin-box__form-label">
+                            Longitude:
+                          </Form.Label>
+                          <Form.Control
+                            className="admin-box__form-input"
+                            name="lng"
+                            
+                            placeholder="Enter coordinate (longitude)"
+                            ref={register}
+                          />
+                          {errors.lng && (
+                            <ErrorMessage>{errors.lng.message}</ErrorMessage>
+                          )}
+                        </Form.Group>
+                      </Col>
+                      <Col lg={4} md={12} sm={12}>
+                        <Form.Group className="admin-box__form-group">
+                          <Form.Label className="admin-box__form-label">
+                            Self Catering:
+                          </Form.Label>
+                          <Form.Control
+                            className="admin-box__form-input admin-box__form-input--select"
+                            as="select"
+                            
+                            ref={register}
+                            name="selfCatering"
+                          >
+                            <option value="true">true</option>
+                            <option value="false">false</option>
+                          </Form.Control>
+                        </Form.Group>
+                      </Col>
+                    </Row>
+                    <Row className="justify-content-md-center">
+                      <Col lg={10} md={6} sm={12}>
+                        <Form.Group>
+                          <Form.Label className="admin-box__form-label">
+                            Description
+                          </Form.Label>
+                          <Form.Control
+                            className="admin-box__form-input"
+                            as="textarea"
+                            rows="5"
+                            name="description"
+                            
+                            placeholder="Enter the establishment description here"
+                            ref={register}
+                          />
+                          {errors.description && (
+                            <ErrorMessage>
+                              {errors.description.message}
+                            </ErrorMessage>
+                          )}
+                        </Form.Group>
+                      </Col>
+                    </Row>
+                    <Row className="justify-content-md-center">
+                      <Col lg={10} md={6} sm={12}>
+                        {validated && (
+                          <div className="admin-box__validation">
+                            <h4>The establishment has been updated successfully!</h4>
+                            
+                          </div>
+                        )}
+                      </Col>
+                    </Row>
+                    <Row className="justify-content-md-end">
+                      <Col lg={4} md={6} sm={12}>
+                        
+                        <button className="admin-box__update-btn" type="submit">
+                          Create
+                        </button>
+                      </Col>
+                    </Row>
+                  </Form>
+                </div>
+              </Col>
+            </Row>
+          </div>
+        </Container>
+      </div>
+    </>
+    );
+}
+
+export default AddEstablishment;
+
+
+/*<Form onSubmit={handleSubmit(onSubmit)}>
             <h1>Add Establishment</h1>
             <Form.Group>
                 <Form.Label>Name</Form.Label>
@@ -35,8 +277,4 @@ function AddEstablishment() {
             </Form.Group>
 
             <Button type="submit">Submit</Button>
-        </Form>
-    );
-}
-
-export default AddEstablishment;
+        </Form> */
--- src/components/admin/establishmentmanager/Establishments.js
+++ src/components/admin/establishmentmanager/Establishments.js
@@ -3,118 +3,104 @@ import { Link } from "react-router-dom";
 import { BASE_URL, headers } from "../../../constants/api";
 import Logo from "../../../assets/logo/logoLight_m.png";
 import { Container, Row, Col, Spinner, Image } from "react-bootstrap";
-import Moment from "react-moment";
 import BackToTop from "../../layout/other/BackToTop";
-import { BsFillHouseDoorFill } from "react-icons/bs"
-
 
 function Establishments() {
-    const [establishments, setEstablishments] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const url = BASE_URL + "establishments";
+  const [establishments, setEstablishments] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const url = BASE_URL + "establishments";
 
-    const options = { headers };
+  const options = { headers };
 
-    useEffect(() => {
-        fetch(url, options)
-            .then((response) => response.json())
-            .then((json) => {
-                console.log(json);
-                setEstablishments(json);
-            })
-            .catch((error) => console.log(error))
-            .finally(() => setLoading(false));
-    }, []);
+  useEffect(() => {
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setEstablishments(json);
+      })
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, []);
 
-    if (loading) {
-        return <div className="spinner-container"><Spinner animation="grow" className="spinner "/>;</div>
-    }
-
+  if (loading) {
     return (
-      <>
-        <div className="main-container main-container main-container--admin">
-          <Container>
-            <div className="basic-btn basic-btn--dark">
-              <Link to={"/admin"}>Dashboard</Link>
-            </div>
-            <div className="admin-box admin-box--enquiry-list">
-              <Row className="justify-content-md-center">
-                <Col lg={3} md={10} sm={12}>
-                  <div className="admin-box__image">
-                    <Image src={Logo} fluid></Image>
-                  </div>
-                </Col>
-              </Row>
-              <Row className="justify-content-md-center">
-                <Col lg={6} md={10} sm={12}>
-                  <h1 className="admin-box__header">Establishments</h1>
-
-                  <div className="admin-box__info-text">
-                    <p>
-                      Ex dolore excepteur sint consequat proident culpa do id.
-                      Labore labore occaecat commodo et.
-                    </p>
-                  </div>
-                  <div className="admin-box__content-list">
-                                <ul>
-                                        
-                                        <li className="admin-box__content-list-item admin-box__content-item--establishments">
-                                            <Link to="/admin/establishments/add">Add Establishment</Link>
-                                        </li>
-                                        
-                                </ul>
-                            </div>
-                </Col>
-              </Row>
-              <Row className="justify-content-md-center">
-                <Col lg={8} md={10} sm={12}>
-                  <div className="admin-box__card-container admin-box__card-container--enquiries">
-                    {establishments.map((establishment) => {
-                      return (
-                        <Link
-                          className="admin-box__card-wrapper"
-                          to={`/admin/establishments/edit/${establishment.id}`}
-                        >
-                          {" "}
-                          <div
-                            key={establishment.id}
-                            className="admin-box__card"
-                          >
-                            <Row className="justify-content-md-center">
-                              <Col lg={2} md={9} sm={9} xs={9}>
-                                <Image fluid src={establishment.image}></Image>
-                              </Col>
-                              <Col lg={10} md={9} sm={9} xs={9}>
-                                <h4 className="admin-box__card-header">
-                                  {establishment.name}
-                                </h4>
-                              </Col>
-                            </Row>
-                          </div>
-                        </Link>
-                      );
-                    })}
-                  </div>
-                </Col>
-              </Row>
-            </div>
-          </Container>
-        </div>
-        <BackToTop />
-      </>
+      <div className="spinner-container">
+        <Spinner animation="grow" className="spinner " />;
+      </div>
     );
+  }
+
+  return (
+    <>
+      <div className="main-container main-container main-container--admin">
+        <Container>
+          <div className="basic-btn basic-btn--dark">
+            <Link to={"/admin"}>Dashboard</Link>
+          </div>
+          <div className="admin-box admin-box--enquiry-list">
+            <Row className="justify-content-md-center">
+              <Col lg={3} md={10} sm={12}>
+                <div className="admin-box__image">
+                  <Image src={Logo} fluid></Image>
+                </div>
+              </Col>
+            </Row>
+            <Row className="justify-content-md-center">
+              <Col lg={6} md={10} sm={12}>
+                <h1 className="admin-box__header">Establishments</h1>
+
+                <div className="admin-box__info-text">
+                  <p>
+                    Ex dolore excepteur sint consequat proident culpa do id.
+                    Labore labore occaecat commodo et.
+                  </p>
+                </div>
+                <div className="admin-box__content-list">
+                  <ul>
+                    <li className="admin-box__content-list-item admin-box__content-item--establishments">
+                      <Link to="/admin/establishments/add">
+                        Add Establishment
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </Col>
+            </Row>
+            <Row className="justify-content-md-center">
+              <Col lg={8} md={10} sm={12}>
+                <div className="admin-box__card-container admin-box__card-container--enquiries">
+                  {establishments.map((establishment) => {
+                    return (
+                      <Link
+                        className="admin-box__card-wrapper"
+                        to={`/admin/establishments/edit/${establishment.id}`}
+                      >
+                        {" "}
+                        <div key={establishment.id} className="admin-box__card">
+                          <Row className="justify-content-md-center">
+                            <Col lg={2} md={9} sm={9} xs={9}>
+                              <Image fluid src={establishment.image}></Image>
+                            </Col>
+                            <Col lg={10} md={9} sm={9} xs={9}>
+                              <h4 className="admin-box__card-header">
+                                {establishment.name}
+                              </h4>
+                            </Col>
+                          </Row>
+                        </div>
+                      </Link>
+                    );
+                  })}
+                </div>
+              </Col>
+            </Row>
+          </div>
+        </Container>
+      </div>
+      <BackToTop />
+    </>
+  );
 }
 
 export default Establishments;
-
-
-/*<h1>Establishments</h1>
-            <ul>
-                {establishments.map((establishment) => {
-                    return (
-                        <li key={establishment.id}>
-                            <NavLink to={`/admin/establishments/edit/${establishment.id}`}>{establishment.name}</NavLink>
-                        </li>
-                    );
-                })}
-            </ul>*/
